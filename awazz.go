cde0e193d4cac364ff42269628c51415ddb14011
Jony Bepary <sohelahmedjony@gmail.com>
2023-10-13 17:04:01 +0600

    fix accidental commit

--- awazz.go
+++ awazz.go
@@ -1,7 +1,26 @@
 package main
 
 // "github.com/SohelAhmedJoni/Awazz-Backend/internal/model"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"message": "pong"})
+	// })
+	// r.GET("/home", homeHandler)
+	// r.GET("/user", UsersHandler)
+	// r.GET("/post", UsersHandler)
+	// r.GET("/notfication", UsersHandler)
+	// r.GET("/notfication", UsersHandler)
+	r.Run() // listen and serve on
+
+	// create a gin server
+	// create a new instance of users handler
+	// create a new instance of users model
+	// create a new instance of users handler
 
 }
