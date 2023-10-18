99e56bbc92089e3a8996cc3515daa20ae2e21dec
Jony Bepary <sohelahmedjony@gmail.com>
2023-10-19 01:49:29 +0600

    #Added ReadFile Function

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,1 +1,16 @@
 package pkg
+
+import (
+	"os"
+)
+
+// read file from path to blob
+func ReadFile(path string) []byte {
+	// Read the contents of the file into a byte slice
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil
+	}
+	return data
+
+}
