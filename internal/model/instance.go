f7a67d5b411095c4bec01e1f93183394ce203e1b
Jony Bepary <sohelahmedjony@gmail.com>
2023-10-21 17:40:47 +0600

    #Added CRUD mechanism for instances to the model

---
+++ internal/model/instance.go
@@ -0,0 +1,40 @@
+package model
+
+import "github.com/SohelAhmedJoni/Awazz-Backend/internal/durable"
+
+func (p *Community) CreateInstance() error {
+
+	db, err := durable.CreateDatabase("./Database/", "common.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	str := `
+	CREATE TABLE IF NOT EXISTS Community (
+		id VARCHAR(128) PRIMARY KEY,
+		instance_id VARCHAR(128),
+		name TEXT,
+		description TEXT,
+		icon TEXT,
+		cover TEXT,
+		created_at TIMESTAMP,
+		updated_at TIMESTAMP,
+		members BIGINT,
+		admins TEXT[],
+		moderators TEXT[],
+		post TEXT[]
+	);`
+	_, err = db.Exec(str)
+	if err != nil {
+		panic(err)
+	}
+	statement, err := db.Prepare("INSERT INTO Community (id, instance_id, name, description, icon, cover, created_at, updated_at, members, admins, moderators, post) VALUES ( ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?)")
+	if err != nil {
+		panic(err)
+	}
+	_, err = statement.Exec(p.Id, p.InstanceId, p.Name, p.Description, p.Icon, p.Cover, p.CreatedAt, p.UpdatedAt, p.Members, p.Admins, p.Moderators, p.Post)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
