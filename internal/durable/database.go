3b942900a8a78f63bee63351c581bac4ed97693a
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2023-10-25 01:18:54 +0530

    Add LeveldbCreateDatabase function

--- internal/durable/database.go
+++ internal/durable/database.go
@@ -8,6 +8,7 @@ import (
 	"path"
 
 	_ "github.com/glebarez/go-sqlite"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // create a database connection to a file path
@@ -29,6 +30,27 @@ func CreateDatabase(params ...string) (*sql.DB, error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return db, err
+}
+func LeveldbCreateDatabase(params ...string) (*leveldb.DB, error) {
+
+	// check if directory exists, create it if it doesn't
+	dir := path.Join(params[0])
+	dir = path.Join(dir, params[1])
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return nil, err
+		}
+	}
+	if len(params) > 1 {
+		dir = path.Join(dir, params[2])
+	}
+	fmt.Println(dir)
+	db, err := leveldb.OpenFile(dir, nil)
+	if err != nil {
+		return nil, err
+	}
+
 	return db, err
 }
 
