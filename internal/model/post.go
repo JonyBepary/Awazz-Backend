2828b1a3143c1ff58c68389ed633f83320544bf5
saria alam <saria.alam21@gmail.com>
2023-10-24 21:08:49 +0600

    Added GET, UPDATE and DELETE func of notifications

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"log"
+	"time"
+
 	"github.com/SohelAhmedJoni/Awazz-Backend/internal/durable"
 )
 
@@ -11,63 +14,69 @@ func (p *Post) SavePost() error {
 	}
 	defer db.Close()
 	str := `
-	CREATE TABLE IF NOT EXISTS Post (
-		Id VARCHAR(128) PRIMARY KEY,
-		Community VARCHAR(128),
-		Content TEXT,
-		CreatedAt TIMESTAMP,
-		UpdatedAt TIMESTAMP,
-		DeletedAt TIMESTAMP,
-		Likes INT,
-	    Shares INT,
-	    Comments INT,
-	    Author VARCHAR(128),
-	    Parent INT,
-	    Rank INT,
-	    IsSensitive bool,
-	    IsNsfw bool,
-	    IsDeleted bool,
-	    IsPinned bool,
-	    IsEdited bool,
-	    IsLiked bool,
-	    IsShared bool,
-	    IsCommented bool,
-	    IsSubscribed bool,
-	    IsBookmarked bool,
-	    IsReblogged bool,
-	    IsMentioned bool,
-	    IsPoll bool,
-	    IsPollVoted bool,
-	    IsPollExpired bool,
-	    IsPollClosed bool,
-	    IsPollMultiple bool,
-	    IsPollHideTotals bool,
-		FragmentationKey VARCHAR(128))
+	CREATE TABLE if not EXISTS POST(
+    Id VARCHAR(255) PRIMARY KEY,
+    Community VARCHAR(255),
+    Content TEXT,
+    CreatedAt INTEGER,
+    UpdatedAt INTEGER,
+    DeletedAt INTEGER,
+    Likes INTEGER,
+    Shares INTEGER,
+    Comments INTEGER,
+    Author VARCHAR(255),
+    Parent VARCHAR(255),
+    Rank INTEGER,
+    Children VARCHAR(255),
+    Tags VARCHAR(255),
+    Mentions VARCHAR(255),
+    IsSensitive BOOLEAN,
+    IsNsfw BOOLEAN,
+    IsDeleted BOOLEAN,
+    IsPinned BOOLEAN,
+    IsEdited BOOLEAN,
+    IsLiked BOOLEAN,
+    IsShared BOOLEAN,
+    IsCommented BOOLEAN,
+    IsSubscribed BOOLEAN,
+    IsBookmarked BOOLEAN,
+    IsReblogged BOOLEAN,
+    IsMentioned BOOLEAN,
+    IsPoll BOOLEAN,
+    IsPollVoted BOOLEAN,
+    IsPollExpired BOOLEAN,
+    IsPollClosed BOOLEAN,
+    IsPollMultiple BOOLEAN,
+    IsPollHideTotals BOOLEAN,
+    FragmentationKey VARCHAR(255)
+)
+
 	`
 	_, err = db.Exec(str)
 	if err != nil {
 		panic(err)
 	}
-	str2 := `
-INSERT INTO Post (
-	Id,Community,Content,CreatedAt,UpdatedAt,DeletedAt,Likes,Shares,Comments,Author,Parent,Rank,IsSensitive,IsNsfw,IsDeleted,IsPinned,IsEdited,IsLiked,IsShared,IsCommented,IsSubscribed,IsBookmarked,IsReblogged,IsMentioned,IsPoll,IsPollVoted,IsPollExpired,IsPollClosed,IsPollMultiple,IsPollHideTotals,FragmentationKey) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);
+	str2 := `INSERT INTO Post (Id,Community,Content,CreatedAt,UpdatedAt,DeletedAt,Likes,Shares,Comments,Author,Parent,Rank,IsSensitive,IsNsfw,IsDeleted,IsPinned,IsEdited,IsLiked,IsShared,IsCommented,IsSubscribed,IsBookmarked,IsReblogged,IsMentioned,IsPoll,IsPollVoted,IsPollExpired,IsPollClosed,IsPollMultiple,IsPollHideTotals,FragmentationKey) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
 	`
 	statement, err := db.Prepare(str2)
 	if err != nil {
 		panic(err)
 	}
-	_, err = statement.Exec(p.Id,
+	_, err = (statement.Exec(p.Id,
 		p.Community,
 		p.Content,
-		p.CreatedAt.String(),
-		p.UpdatedAt.String(),
-		p.DeletedAt.String(),
+		p.CreatedAt,
+		p.UpdatedAt,
+		p.DeletedAt,
 		p.Likes,
 		p.Shares,
 		p.Comments,
 		p.Author,
 		p.Parent,
 		p.Rank,
+		p.Children,
+		p.Tags,
+		p.Mentions,
 		p.IsSensitive,
 		p.IsNsfw,
 		p.IsDeleted,
@@ -86,37 +95,70 @@ INSERT INTO Post (
 		p.IsPollClosed,
 		p.IsPollMultiple,
 		p.IsPollHideTotals,
-		p.IsPollHideTotals,
 		p.FragmentationKey,
-	)
+	))
 	if err != nil {
 		panic(err)
 	}
 
 	return nil
 }
-func (p *Post) GetPost(postid string) error {
-	db, err := durable.CreateDatabase("./Database/", "Common", "Shard_0.sqlite")
+func (p *Post) GetPost(msgId string) error {
+	db, err := durable.CreateDatabase("Database/", "Common", "Shard_0.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// spew.Dump(rows)
+	rows, err := db.Query("SELECT * FROM ")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(p.Id, p.Community, p.Content, p.CreatedAt, p.UpdatedAt, p.DeletedAt, p.Likes, p.Shares, p.Comments, p.Author, p.Parent, p.Rank, p.Children, p.Tags, p.Mentions, p.IsSensitive, p.IsNsfw, p.IsDeleted, p.IsPinned, p.IsEdited, p.IsLiked, p.IsShared, p.IsCommented, p.IsSubscribed, p.IsBookmarked, p.IsReblogged, p.IsMentioned, p.IsPoll, p.IsPollVoted, p.IsPollExpired, p.IsPollClosed, p.IsPollMultiple, p.IsPollHideTotals, p.FragmentationKey)
+		if err != nil {
+			panic(err)
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+func (u *Post) UpdatePost(ID string) error {
+	db, err := durable.CreateDatabase("./Database/", "common", "Shard_0.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	u.UpdatedAt = time.Now().Unix()
+
+	_, err = db.Exec("UPDATE POST SET UpdatedAt = ?, Content = ? WHERE 	Id = ? ", u.UpdatedAt, u.Content, ID)
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer db.Close()
+
 	return nil
 }
-func (p *Post) UpdatePost(postid string) error {
-	db, err := durable.CreateDatabase("./Database/", "Common", "Shard_0.sqlite")
+func (d *Post) DeletePost(Id string) error {
+	db, err := durable.CreateDatabase("./Database/", "common", "Shard_0.sqlite")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	return nil
-}
-func (p *Post) DeletePost(postid string) error {
-	db, err := durable.CreateDatabase("./Database/", "Common", "Shard_0.sqlite")
+
+	_, err = db.Exec("DELETE FROM  POST WHERE  Id= ?", Id)
+
+	d.UpdatedAt = time.Now().Unix()
+
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer db.Close()
 	return nil
 }
