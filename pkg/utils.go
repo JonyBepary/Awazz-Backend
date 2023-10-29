f1ce06ad8854ec2d31a2b88da7f6ebd1853feebe
Sohel Ahmed Jony <sohelahmedjony@gmail.com>
2023-10-30 01:22:45 +0600

    #added ULID and Hash generation function

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,10 +1,21 @@
 package pkg
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"os"
+	"time"
+
+	"golang.org/x/exp/rand"
 
 	"github.com/SohelAhmedJoni/Awazz-Backend/internal/model"
+	ulid "github.com/oklog/ulid/v2"
 )
+
+func GetUlid() string {
+	r := rand.New(new(rand.LockedSource))
+	return ulid.MustNew(ulid.Timestamp(time.Now()), ulid.Monotonic(r, 0)).String()
+}
 
 // read file from path to blob
 func ReadFile(path string) []byte {
@@ -37,4 +48,20 @@ func WriteServerPKI() (*model.AKS, error) {
 		return nil, err
 	}
 	return pk, nil
+}
+
+func StringHashGeneration(str string) string {
+	digest := sha256.New()
+	digest.Write([]byte(str))
+	return fmt.Sprintf("%x", digest.Sum(nil))
+}
+
+func FileHashGeneration(filename string) string {
+	hash := sha256.New()
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+	hash.Write([]byte(fmt.Sprintf("%v", file)))
+	return fmt.Sprintf("%x", hash.Sum(nil))
 }
